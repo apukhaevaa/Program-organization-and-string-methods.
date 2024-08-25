6f9933b16de918aafdbd8e5853a536867a1eee9d
apukhaevaa <apukhaevaa@gmail.com>
2024-08-25 21:18:30 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+# Пухаева Алина Александровна
+# Program organization and string methods.
+# 25.08.2024
+my_string = input('Введите инициалы любимого актёра? ')
+print(len('Название любимого фильма? '))
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
